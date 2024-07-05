d9c98f0b7ebcecd661e9890c3ee81fb76378555e
Konrad1991 <konrad_kraemer@yahoo.de>
2024-07-05 10:01:53 +0200

    update translate.R to new version of ast2ast

--- R/translate.R
+++ R/translate.R
@@ -1,55 +1,46 @@
-
 R2cpp <- function(ode) {
-  stopifnot("ode is not of type function"=is.function(ode))
-  odefcpp <- ast2ast::translate(ode, output = "XPtr",
-                                types_of_args = c("double", rep("sexp", 4)),
-                                reference = TRUE,
-                                return_type = "void",
-                                getsource = TRUE)
-  
+  stopifnot("ode is not of type function" = is.function(ode))
+  odefcpp <- ast2ast::translate(ode,
+    references = rep(TRUE, 5),
+    types_of_args = c("double", "const double", rep("double", 3)),
+    data_structures = c("scalar", rep("borrow", 4)),
+    handle_inputs = rep("", 5),
+    output = "XPtr", getsource = TRUE
+  )
+
   odefcpp <- strsplit(odefcpp, "\n")[[1]]
   odetmp <- character(length(odefcpp))
   counter <- 1
-  for(i in odefcpp) {
-    if("SEXP getXPtr() {" == i) break
+  for (i in odefcpp) {
+    if ("SEXP getXPtr ()  {" == i) break
     odetmp[counter] <- i
     counter <- counter + 1
   }
   odetmp <- paste(odetmp, collapse = "\n")
-  
-  fctptr <- '
+
+  fctptr <- "
   int wrapper_ode(double t, const arma::vec& y, arma::vec& ydot,
                 Rcpp::RObject& param, Rcpp::NumericVector& psens) {
-    sexp y_ = etr::vector(0.0, y.size());
-    for(int i = 0; i < y.size(); i++) {
-      y_[i] = y[i];
-    }
-    sexp ydot_(ydot.size(), ydot.memptr(), 2);
+    etr::Vec<const double, etr::Borrow<const double>> y_(y.memptr(), y.size());
+    etr::Vec<double, etr::Borrow<double>> ydot_(ydot.memptr(), ydot.size());
     Rcpp::NumericVector p(param);
-    sexp param_(p.size(), p.begin(), 2);
+    etr::Vec<double, etr::Borrow<double>> param_(p.begin(),p.size());
     Rcpp::NumericVector ps(psens);
-    sexp psens_(ps.size(), ps.begin(), 2);
+    etr::Vec<double, etr::Borrow<double>> psens_(ps.begin(),ps.size());
     ode(t, y_, ydot_, param_, psens_);
     return(0);
   }
-  SEXP getXPtr() {                                                                                                   
+  SEXP getXPtr() {
   typedef int (*fct_ptr) (
                 double t, const arma::vec& y, arma::vec& ydot,
-                Rcpp::RObject& param, Rcpp::NumericVector& psens);                        
-  return Rcpp::XPtr<fct_ptr>(new fct_ptr(&  wrapper_ode ));                                                                  
-  }    
-  '
-
+                Rcpp::RObject& param, Rcpp::NumericVector& psens);
+  return Rcpp::XPtr<fct_ptr>(new fct_ptr(&  wrapper_ode ));
+  }
+  "
   ode <- paste(odetmp, fctptr)
-
   env <- new.env()
-  Rcpp::sourceCpp(code = ode, verbose = TRUE, env = env) 
+  Rcpp::sourceCpp(code = ode, verbose = TRUE, env = env)
   fct_ret <- env$getXPtr()
   attributes(fct_ret) <- list(class = "XPtr")
-
   return(fct_ret)
 }
-
-
-
-
